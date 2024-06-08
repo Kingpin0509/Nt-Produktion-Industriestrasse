8fb9a46196ae098a6c128c4c5a5c225b177091b0
Kingpin0509 <Kingpin0509@gmail.com>
2024-06-08 21:04:28 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.nietietproduktion',
+  appName: 'nietietproduktion',
+  webDir: 'www'
+};
+
+export default config;
